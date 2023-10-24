f4d57db49f85dfeef26df49d11272ad72346f41b
DK <dasha.krutsko@gmail.com>
2023-10-24 21:40:50 +0300

    version 1

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -1,13 +1,25 @@
 package com.mjc.stage2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShopStock {
     private final List<Product> productList;
 
-    public ShopStock(List<Product> productList) {
+    public ShopStock(List<Product> productList)
+    {
         this.productList = productList;
     }
 
-    // Write your code here!
+    public List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy){
+        List<Product> filteredProductList = new ArrayList<>();
+
+        for (Product product:productList){
+            if (filteringStrategy.filter(product)){
+                filteredProductList.add(product);
+            }
+        }
+        return filteredProductList;
+    }
+
 }
